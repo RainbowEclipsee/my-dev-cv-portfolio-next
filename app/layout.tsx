df4f1796c6ae49a25ef5c9bbbc41ff788d6a0114
RainbowEclipsee <dexterrullez1@gmail.com>
2025-07-01 17:52:46 +0300

    upd - metadata tags for social networks

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,34 @@ import "@/styles/main.css";
 
 export const metadata: Metadata = {
   title: "Bogdan Makarchuk | Frontend Developer",
-  description: "Персональное портфолио разработчика: проекты, навыки и контакты",
+  description: "Персональное портфолио разработчика: проекты, навыки, стек и контактная информация.",
+  openGraph: {
+    title: "Bogdan Makarchuk | Frontend Developer",
+    description: "Персональное портфолио разработчика: проекты, навыки, стек и контактная информация.",
+    url: "https://makarchuk-tech.vercel.app/",
+    siteName: "Bogdan Makarchuk | Frontend Developer",
+    images: [
+      {
+        url: "https://makarchuk-tech.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Bogdan Makarchuk Portfolio Preview",
+        type: "image/png",
+      },
+    ],
+    locale: "ru_RU",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bogdan Makarchuk | Frontend Developer",
+    description: "Персональное портфолио разработчика: проекты, навыки, стек и контактная информация.",
+    images: ["https://makarchuk-tech.vercel.app/og-image.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
